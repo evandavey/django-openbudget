8fd2afda5acea0ef8fd7a92dbdc828b1b465cfaa
Evan Davey <evan.j.davey@gmail.com>
2011-10-07 16:22:28 +0100

    Adds account balance property

--- openbudgetapp/models/account.py
+++ openbudgetapp/models/account.py
@@ -1,7 +1,6 @@
 from django.db import models
-
-# Create your models here.
-
+from openbudgetapp.models.split import Split
+import numpy as np
 
 class Account(models.Model):
 
@@ -12,6 +11,37 @@ class Account(models.Model):
 	guid=models.CharField(max_length=32,primary_key=True)
 	name=models.CharField(max_length=2048)
 	account_type=models.CharField(max_length=2048)
-	parent=models.ForeignKey('Account')
+	parent=models.ForeignKey('Account',null=True)
 	
 	
+		
+	def __unicode__(self):
+		""" Returns the custom output string for this object
+		"""
+		return "%s (%s)" % (self.name,self.account_type) 
+	
+	
+	def splits(self):
+		
+		qs=Split.objects.filter(account=self)
+
+		if len(qs)==0:
+			return None
+		vlqs = qs.values_list()
+		splits = np.core.records.fromrecords(vlqs, names=[f.name for f in Split._meta.fields])
+		
+		return splits
+	
+	@property
+	def balance(self):
+		
+		splits=self.splits()
+		
+		if splits is None:
+			return 0
+		
+		return splits.value.sum()
+		
+	
+		
+		
