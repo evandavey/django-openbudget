84491630306e507a55934968daf4262827e7719c
Evan Davey <evan.j.davey@gmail.com>
2011-10-07 16:22:04 +0100

    Adds transaction and split to admin

---
+++ openbudgetapp/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
+
+from openbudgetapp.models import *
+
+class AccountAdmin(admin.ModelAdmin):
+	""" Object to control the behaviour of the linked object in the Admin interface
+	"""
+	list_display = ['guid','name','balance','account_type','parent']
+	list_filter = ['account_type']
+	ordering = ['name']
+	search_fields = ['name']
+
+
+class TransactionAdmin(admin.ModelAdmin):
+	""" Object to control the behaviour of the linked object in the Admin interface
+	"""
+	list_display = ['guid','postdate','description']
+	list_filter = ['postdate']
+	ordering = ['postdate']
+	search_fields = ['description']
+
+
+class SplitAdmin(admin.ModelAdmin):
+	""" Object to control the behaviour of the linked object in the Admin interface
+	"""
+	list_display = ['guid','tx','account','value']
+	list_filter = []
+	ordering = []
+	search_fields = []
+
+admin.site.register(Account,AccountAdmin)	
+admin.site.register(Transaction,TransactionAdmin)	
+admin.site.register(Split,SplitAdmin)	
+
+
+
+
+
+
+	
