5283aa05b1365b858d65c668d87eaed732362ffe
Evan Davey <evan.j.davey@gmail.com>
2012-01-24 11:37:24 +0000

    switches to mysql for staging

--- settings_staging.py
+++ settings_staging.py
@@ -5,5 +5,9 @@ DEBUG = True
 
 STATIC_ROOT = '/usr/local/web/django/www/staging/openbudget/static'
 
-DATABASE_ENGINE = 'sqlite3'
-DATABASE_NAME = '/usr/local/web/django/db/openbudget.db'
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = 'staging-openbudget'             # Or path to database file if using sqlite3.
+DATABASE_USER = 'openbudget'             # Not used with sqlite3.
+DATABASE_PASSWORD = 'openbudget'         # Not used with sqlite3.
+DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
