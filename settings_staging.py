48efeffa23654823af7541eeaf1e76edef99794c
Evan Davey <evan.j.davey@gmail.com>
2012-01-24 11:41:58 +0000

    fixes db name

--- settings_staging.py
+++ settings_staging.py
@@ -6,7 +6,7 @@ DEBUG = True
 STATIC_ROOT = '/usr/local/web/django/www/staging/openbudget/static'
 
 DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'staging-openbudget'             # Or path to database file if using sqlite3.
+DATABASE_NAME = 'staging_openbudget'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'openbudget'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'openbudget'         # Not used with sqlite3.
 DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
