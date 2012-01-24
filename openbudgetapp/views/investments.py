564b54a5e0f7a05d5bb00cbef8fb77db2dd8ac7e
Evan Davey <evan.j.davey@gmail.com>
2012-01-24 17:30:19 +0000

    fixes bug

--- openbudgetapp/views/investments.py
+++ openbudgetapp/views/investments.py
@@ -140,13 +140,12 @@ def report(request,enddate=None,startdate=None,format='html'):
 	        interest=interest[(interest.index>startdate+timedelta(days=5)) & (interest.index<enddate+timedelta(days=5))].sum()
 	        r=interest/start
 	        r=1
-        except:
-            print '...error calculating' 
-            r=0
-
-        if r:
-            print '...effective rate %.2f' % r
-            i_data.append({'account':i.name,'interest':interest,'rate':r*400,'start':start,'end':end})
+	    except:
+	        print '...error calculating' 
+	        r=0
+	    if r:
+	        print '...effective rate %.2f' % r
+	        i_data.append({'account':i.name,'interest':interest,'rate':r*400,'start':start,'end':end})
 	    
 
    
